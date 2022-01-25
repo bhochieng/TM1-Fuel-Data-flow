3dcc3987cc58cd5e862bbeff8ff8562b8921b0ef
Ochieng <Humphrey.Ochieng@kenya-airways.com>
2022-01-25 11:44:29 +0300

    Changed auth structure

--- read_MA.py
+++ read_MA.py
@@ -5,6 +5,7 @@ import cx_Oracle
 import glob
 import re
 import subprocess
+from datetime import datetime
 
 with open('passwords/credentials.json') as json_data:
     credentials = json.load(json_data)
@@ -15,6 +16,7 @@ db_username = credentials['authentication']['dwh_credentials']['db_user']
 db_password = credentials['authentication']['dwh_credentials']['db_password']
 db_port = credentials['authentication']['dwh_credentials']['db_port']
 oracle_sid = credentials['authentication']['dwh_credentials']['oracle_sid']
+
 #check plafrom
 if platform.system().upper() == 'LINUX':
     os.environ['LD_LIBRARY_PATH'] = credentials['authentication']['dwh_credentials']['ld_library_path']
@@ -28,33 +30,82 @@ else:
     dsn_tns = cx_Oracle.makedsn(db_host, db_port, oracle_sid)
     dbCon = cx_Oracle.connect(db_username, db_password, dsn_tns)
 cursor = cx_Oracle.Cursor(dbCon) 
+
+
+cursor = cx_Oracle.Cursor(dbCon)
 cursor1 = cx_Oracle.Cursor(dbCon)
 cursor2 = cx_Oracle.Cursor(dbCon)
 cursor3 = cx_Oracle.Cursor(dbCon)
 cursor4 = cx_Oracle.Cursor(dbCon)
 cursor_select   = cx_Oracle.Cursor(dbCon)
+_k = 1
+digits = frozenset('0123456789-+')
 
+lines =''
+_mdate = ''
+_mdoc = ''
+_myesterday  = ''
+today_date = datetime.datetime.today()
+tomorrows_date = today_date + datetime.timedelta(0)
+_mprocdate =  (today_date).strftime('%d-%b-%Y')
+myesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
+myesterday1 = datetime.datetime.now() - datetime.timedelta(days = 1)    
+myesterday=  myesterday.strftime("%y%m")
+myesterday1=  myesterday1.strftime("%y%m%d")
+
+##dsn_tns = cx_Oracle.makedsn('10.2.155.78', 1521, 'warehouse')
+
+_mopen=0
 def main():
     _mcount = 0
+    _monceerror = 0
+    _mopen=0
     while _mcount <1:
         if platform.system().upper() == 'LINUX':
+            #_mtype = '1004.TXT'
+            _merrorlogpath = '/u01/projects/MA_project/data/'
             _msoucefolder = '/u01/projects/AIMS_Files/attachments/'
+            _moutputfolder = '/u01/projects/MA_project/data/'
+            _mbackupfolder = '/u01/projects/MA_project/data/'
+	    print('HEREEEE')
         else:
             _msoucefolder = 'C:\\Files\\'
+            _moutputfolder = 'C:\\Files\\Output\\'
+            _merrorlogpath  = 'C:/Files/'
+            _mbackupfolder = 'C:\\Files\\processed\\'
+        _mopen = 0
         rows=[]
-        _mtype = _msoucefolder+'*MA.txt'
+        #_moutfile = 'MA_NEW.txt'
+        _mtype = _msoucefolder+'*MA*.txt'
+        #_mtype = '/app/u02/businessobjects/dataservices/amadeus_hot/altea/PRD.TSR.KQ.AL.HOT.D'+_myesterday
         for filename in glob.glob(_mtype):
+            filelogl=[]
+	    print('Here 2',filename )
+            head, filename1 = os.path.split(filename)
             mfilename =  os.path.basename(filename )
             mfilename_db = mfilename[:-4]
             mfilename_db = mfilename[:19]
+            mfile_type = mfilename[20:-1]
+            _merrorfile = _merrorlogpath+mfilename+'.log'
+
+            _mfilename = filename
             _monce = 0
+            _mnorecords = 0
+            _mfileloaded = 0
             fh  = open(filename,'r')
             _n = 0
+	    print('Here 3',filename )
             for lines in fh:
-                [_mdate,_mflt,_mcarrier,_mtype,_mreg,_mdep,_marr,_mstd,_msta,_metd,_meta,_mtkof,_mtdwn,_matd,_mata,_mblock,_mflthr,_mst,_mstd_local,_msta_local,_metd_local,_meta_local,_mtkof_local,_mtdwn_local,_matd_local,_mata_local,_macversion,_macconfig,_mactpax,_mpax,_mactblockoff,_macttkoff,_macttdown,_mactblockon,_morgdate,_morgstd,_morgsta,_mdly1,_mdly1arr,_minitia,_muplift,_mtotal,_mremain,_maburn,_msg,_mcargo,_mmail,_mbags,_mcrew ] =['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']
+                #print(lines)
+                #[_Mfltnum,_Mdept,_Mdest,_mtailNum,_MactEqpmt,_MblockTime,_MflightTime,_Mdepdate,_Mdeptime,_Moffdate,_Mofftime,_Mtakeoffdate,_Mtakeofftime,_Mtouchdowndate,_Mtouchdowntime,_Monblockdate,_Monblocktime,_MoutLT,_MoffLT,_MonLT,_MinLT,_Mstdgmtvr,_Mstagmtvr,_MactYLPax,_MactYLPad,_MactYLAwd,_MactYLInf,_MactYRPax,_MactYRPad,_MactYRAwd,_MactYRInf,_MactYTPax,_MactYTPad,_MactYTAwd,_MactYTInf,_McatAdult,_McatMen,_McatWomen,_McatChild,_McatInfant,_McatAll,_McatTurn,_MxAlc,_MdeptFuel,_MarrvFuel,_MonFuel,_Mmxconsumpt,_MupliftQty,_MdeFuelQty,_Mdenstity,_MtaxiInFuel,_MoffFuel,_MactJPax ,_MactJPad ,_MactJAwd,_McreateDate] =['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']
+                [_mdate,_mflt,_mcarrier,_mtype,_mreg,_mdep,_marr,_mstd,_msta,_metd,_meta,_mtkof,_mtdwn,_matd,_mata,_mblock,_mflthr,_mst,_mstd_local,_msta_local,_metd_local,_meta_local,_mtkof_local,_mtdwn_local,_matd_local,_mata_local,_macversion,_macconfig,_mactpax,_mpax,_mactblockoff,_macttkoff,_macttdown,_mactblockon,_morgdate,_morgstd,_morgsta,_mdly1,_mdly1arr,_minitia,_muplift,_mtotal,_mremain,_maburn,_msg,_mcargo,_mmail,_mbags,_mcrew ,_McreateDate ] =['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']
+                #lines = lines .replace(",", ";")
                 mlines = lines.split("\t")
                 _mrow = []
+                print(len(mlines))
+                # the data is 41
                 if len(mlines) == 49 and not mlines[0].strip()=='DATE' and mlines[0].strip()  and not mlines[2].strip() == 'PW':   
+                    #print(mlines[0].strip())
                     _mdate      =     mlines[0].strip()
                     _mflt      =     mlines[1].replace('*','').strip()
                     _mcarrier      =     mlines[2].strip()
@@ -108,15 +159,21 @@ def main():
                     _mmail      =     mlines[46]
                     _mbags      =     mlines[47]
                     _mcrew  =     mlines[48]
+                    _McreateDate = _mprocdate     # TRANS_DATE
                     _mfiledatetime = mfilename_db.strip()
-
-                    ####Add gmt var and time zones based on airport IATA CODES.
-                    #get  timezone for dep
+                    #extra fields.
+                    if mfile_type.strip() == 'MA.tx':
+                        _mfile_type = 'NORMAL'
+                    elif mfile_type.strip() == 'MA_A.tx':
+                        _mfile_type = 'ONDEMAND'
+                    else:
+                        _mfile_type = 'NORMAL FILE'
                     _utc_timezone = """ select TIMEZONE from AIRPORT_TZ_OFFSET where AIRPORT_CODE = :airport_code """
                     cursor1.execute(_utc_timezone,airport_code=_mdep)
-                    #get  timezone for arr
+
                     _utc_timezone = """ select TIMEZONE from AIRPORT_TZ_OFFSET where AIRPORT_CODE = :airport_code """
                     cursor2.execute(_utc_timezone,airport_code=_marr)
+
                     for row in cursor1:
                         _flt_dep_gmt_time_zone = str(row[0]).strip()
                         # print(_flt_dep_gmt_time_zone)
@@ -135,23 +192,28 @@ def main():
                     for row in cursor4:
                         _flt_arr_gmt_var = str(row[0]).replace(':','').strip()
                         # print(_flt_dep_gmt_var)
-                    print("DEP",_flt_dep_gmt_time_zone ,_flt_dep_gmt_var, "|     ARR", _flt_arr_gmt_time_zone,_flt_arr_gmt_var )
-
-                    _mrow= [_mdate,_mflt,_mcarrier,_mtype,_mreg,_mdep,_marr,_mstd,_msta,_metd,_meta,_mtkof,_mtdwn,_matd,_mata,_mblock,_mflthr,_mst,_mstd_local,_msta_local,_metd_local,_meta_local,_mtkof_local,_mtdwn_local,_matd_local,_mata_local,_macversion,_macconfig,_macconfig1,_mactpax,_mactpax1,_mpax,_mactblockoff,_macttkoff,_macttdown,_mactblockon,_morgdate,_morgstd,_morgsta,_mdly1,_mdly1arr,_minitia,_muplift,_mtotal,_mremain,_maburn,_msg,_mcargo,_mmail,_mbags,_mcrew ,_McreateDate,_mfiledatetime,_flt_dep_gmt_var,_flt_arr_gmt_var,_flt_dep_gmt_time_zone,_flt_arr_gmt_time_zone ]
+                    # print("DEP",_flt_dep_gmt_time_zone ,_flt_dep_gmt_var, "|     ARR", _flt_arr_gmt_time_zone,_flt_arr_gmt_var )
+                    
+                    _mrow= [_mdate,_mflt,_mcarrier,_mtype,_mreg,_mdep,_marr,_mstd,_msta,_metd,_meta,_mtkof,_mtdwn,_matd,_mata,_mblock,_mflthr,_mst,_mstd_local,_msta_local,_metd_local,_meta_local,_mtkof_local,_mtdwn_local,_matd_local,_mata_local,_macversion,_macconfig,_macconfig1,_mactpax,_mactpax1,_mpax,_mactblockoff,_macttkoff,_macttdown,_mactblockon,_morgdate,_morgstd,_morgsta,_mdly1,_mdly1arr,_minitia,_muplift,_mtotal,_mremain,_maburn,_msg,_mcargo,_mmail,_mbags,_mcrew ,_McreateDate,_mfiledatetime,_flt_dep_gmt_var,_flt_arr_gmt_var,_flt_dep_gmt_time_zone,_flt_arr_gmt_time_zone,_mfile_type ]
+                    
                     rows.append(_mrow)
                     print(_mrow)
                     _monce ==1
                     _n = _n + 1
             if _n>0 :
-                cursor.prepare('insert into system.T_TM1_LOAD(date_,flt,carrier,type,reg,dep,arr,std,sta,etd,eta,tkof,tdwn,atd,ata,block,flthr,st,std_local,sta_local,etd_local,eta_local,tkof_local,tdwn_local,atd_local,ata_local,acversion,BUSINESS_CLASS,ECONOMY,ACTPAX_BUSINESS,ACTPAX_ECONOMY,pax,actblockoff,acttkoff,acttdown,actblockon,orgdate,orgstd,orgsta,dly1,dly1arr,initia,uplift,total,remain,aburn,sg,cargo,mail,bags,crew,createdate,EMAIL_FILE_DATE,DEP_GMT_VAR,ARR_GMT_VAR,DEP_TIMEZONE,ARR_TIMEZONE) values (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14,:15,:16,:17,:18,:19,:20,:21,:22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32, :33, :34, :35, :36, :37, :38, :39, :40,:41,:42,:43,:44,:45,:46,:47,:48,:49,:50,:51,:52,:53,:54,:55,:56,:57)')
+                cursor.prepare('insert into dwh_foundation.T_TM1_NEW1_1_LOAD(date_,flt,carrier,type,reg,dep,arr,std,sta,etd,eta,tkof,tdwn,atd,ata,block,flthr,st,std_local,sta_local,etd_local,eta_local,tkof_local,tdwn_local,atd_local,ata_local,acversion,BUSINESS_CLASS,ECONOMY,ACTPAX_BUSINESS,ACTPAX_ECONOMY,pax,actblockoff,acttkoff,acttdown,actblockon,orgdate,orgstd,orgsta,dly1,dly1arr,initia,uplift,total,remain,aburn,sg,cargo,mail,bags,crew,createdate,EMAIL_FILE_DATE,STD_GMTVR,STAGMTVR,DEP_TIMEZONE,ARR_TIMEZONE,FILE_TYPE) values (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14,:15,:16,:17,:18,:19,:20,:21,:22, :23, :24, :25, :26, :27, :28, :29, :30, :31, :32, :33, :34, :35, :36, :37, :38, :39, :40,:41,:42,:43,:44,:45,:46,:47,:48,:49,:50,:51,:52,:53,:54,:55,:56,:57,:58)')
+                #cursor.prepare('insert into DWH_FOUNDATION.T_MA(DAY_FLT,FLT,DEP,ARR,REG,EXPPAS,TRANSIT,PASON,PASOFF,TOTAL_PAX,AC,BLOF,BLON,TKOF,ATD_LOCAL,ATA_LOCAL,TDOWN,LEGSTATUS,ACTUAL_BLOCK_TIME,FLIGHT_TIME_MVT,MVT) values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14,:15,:16,:17,:18,:19,:20,:21)')
                 cursor.executemany(None, rows)
                 rows=[]
                 _n = 0
                 dbCon.commit()
-        _mcount = _mcount + 1        
+
+        _mcount = _mcount + 1
+        
 if __name__ == "__main__" : main()
 if platform.system().upper() == 'LINUX':
     subprocess.call(['./mv_ma_files.sh']) 
+    execfile("/u01/projects/TM1_Fuelplus_Projects/scripts/insertintotm1new1.py")
     print('Done processing')
 else:
     print('Done processing,')
